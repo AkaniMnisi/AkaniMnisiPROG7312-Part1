708632b43003b361570f637fdc06fd333142c8b8
AkaniMnisi <akanimnisi26@gmail.com>
2026-09-14 19:37:43 +0200

    Added Sensor Data Class
    File added

---
+++ SmartX System/SmartX System/Models/SensorData.cs
@@ -0,0 +1,22 @@
+﻿namespace SmartX_System.Models
+{
+    public class SensorData
+    {
+        public string SensorId { get; set; } = string.Empty;
+        public double Value { get; set; }
+
+        
+        public static SensorData operator +(SensorData a, SensorData b)
+        {
+            return new SensorData
+            {
+                SensorId = $"{a.SensorId}+{b.SensorId}",
+                Value = a.Value + b.Value
+            };
+        }
+
+       
+        public static bool operator >(SensorData a, SensorData b) => a.Value > b.Value;
+        public static bool operator <(SensorData a, SensorData b) => a.Value < b.Value;
+    }
+}
--- SmartX System/SmartX System/Models/TelemetryPacket.cs
+++ SmartX System/SmartX System/Models/TelemetryPacket.cs
@@ -1,6 +1,15 @@
 ﻿namespace SmartX_System.Models
 {
-    public class TelemetryPacket
+    public class TelemetryPacket<T>
     {
+        public string SensorId { get; set; } = string.Empty;
+        public string MacAddress { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public T? Payload { get; set; } // Generic type T
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+       
+        public List<string> AttachedLogs { get; set; } = new List<string>();
     }
 }
