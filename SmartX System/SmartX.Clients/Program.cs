d82206bbcf62dda2530a0b4f677d47cfb07a6784
AkaniMnisi <akanimnisi26@gmail.com>
2026-09-15 00:09:32 +0200

    updating files
    files updated

---
+++ SmartX System/SmartX.Clients/Program.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SmartX.Clients;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// IMPORTANT: Change 5001 to match the port of your API (Check SmartX System/Properties/launchSettings.json)
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5001/") });
+
+await builder.Build().RunAsync();
